0147458b1d55a7b3eb8937bee83275bfa9540459
Michael Braun <michaelbraunbass@gmail.com>
2018-08-23 14:26:07 -0600

    JSON Working with Test Data

--- scanfile.py
+++ scanfile.py
@@ -18,12 +18,11 @@ requests.packages.urllib3.disable_warnings()
 
 #Scan file method
 def scanfile(api_key, url, te_cookie):
-    file_path = raw_input("Enter full file path (Use forward slashes, even in Windows): ")
+    file_path = raw_input("Enter full file path (Use forward slashes, even in Windows):")
     
- 
+    print "\n"
    
-    data = ''' 
-    {
+    data = {
         "request": [
             {
                 "md5": "8dfa1440953c3d93daafeae4a5daa326",
@@ -53,19 +52,14 @@ def scanfile(api_key, url, te_cookie):
             }
         ]
     }
-    '''
-    
-    data_json = json.loads(data)
-    headers = {
-            "User-Agent": "python-api-wrapper",
-            "Accept": "*/*",
-            "Content-Type": "application/json", 
-            "Content-Length": len(data),
-            "Authorization": api_key,
-            "te_cookie": te_cookie
-        }
     
     
-    response = requests.post(url+"query", json=data_json, headers=str(headers), verify=False)
-    print response
+    headers = {"User-Agent": "python-api-wrapper", "Accept": "*/*","Content-Type": "application/json", "Content-Length": str(len(data)),"Authorization": api_key,}
     
+    #try:
+    print "Scanning Now..."
+    response = requests.post(url+"query", json=data, headers=headers, verify=False)
+    response_json = json.loads(response.content)
+    print response_json
+    #except:
+    #   print "Query Failed. Please try again."
